115bdf355ccb4622ca37fbda8a14a1b489985617
Thiago Alves <thiagoralves@gmail.com>
2023-01-26 10:34:24 -0500

    Fix missing "method" parameter on connect_to_tcp_server

--- webserver/core/lib/openplc_networking.h
+++ webserver/core/lib/openplc_networking.h
@@ -1,5 +1,5 @@
 //client.cpp
-int connect_to_tcp_server(uint8_t *ip_address, uint16_t port);
+int connect_to_tcp_server(uint8_t *ip_address, uint16_t port, int method);
 int send_tcp_message(uint8_t *msg, size_t msg_size, int socket_id);
 int receive_tcp_message(uint8_t *msg_buffer, size_t buffer_size, int socket_id);
 int close_tcp_connection(int socket_id);
