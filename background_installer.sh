3f61c39f3d9e3d3800e6ea131e3421f3402f3aab
Thiago Alves <thiagoralves@gmail.com>
2025-01-18 11:00:50 -0500

    Brought back "active_program" that was missing in persistent folder

--- background_installer.sh
+++ background_installer.sh
@@ -262,6 +262,7 @@ if [ -d "/docker_persistent" ]; then
     mkdir -p /docker_persistent/st_files
     cp -n /workdir/webserver/dnp3_default.cfg /docker_persistent/dnp3.cfg
     cp -n /workdir/webserver/openplc_default.db /docker_persistent/openplc.db
+    cp -n /workdir/webserver/active_program_default /docker_persistent/active_program
     cp -n /workdir/webserver/st_files_default/* /docker_persistent/st_files/
     cp -n /dev/null /docker_persistent/persistent.file
     cp -n /dev/null /docker_persistent/mbconfig.cfg
